64ea4acace81753745e4d89bba7e69da604319f9
Drevanok <sauleliseoaparicio@gmail.com>
2026-06-06 00:01:57 -0700

    feat: add DatePipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,5 +11,18 @@ export default class BasicPage {
   nameLower = signal('saul');
   nameUpper = signal('SAUL');
   fullName = signal('SauL AParIciO');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+      console.log('thick')
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+  })
 
 }
